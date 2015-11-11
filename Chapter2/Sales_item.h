1290cb4ae99c210a879291f12064a6a79e5e342f
q9liu <qiang.5.liu@nokia.com>
2015-11-11 10:15:06 +0100

    x

---
+++ Chapter2/Sales_item.h
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Sales_item
+{
+    public:
+        // Operations on sales item objects will go here
+
+    private:
+        // datas or attributes on this class
+        string ibsn;
+        unsigned int units_sold;
+        double revenue;
+};
