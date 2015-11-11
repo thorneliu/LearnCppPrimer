14607d1551229f9b8db7b2ca2be051478c29d20e
q9liu <qiang.5.liu@nokia.com>
2015-11-11 10:43:17 +0100

    add

---
+++ Chapter3/note.md
@@ -0,0 +1,9 @@
+string 类型的输入操作符  
+>* 忽略开头的所有空白字符(空格， 换行符，制表符)
+>* 读取字符直至再次遇到空白字符，读取终止
+
+```C
+ cin >> string; //If "Hello World" is inputed, 
+ // string is "Hello"  
+```
+
---
+++ Chapter3/string.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std; 
+//using std::istream;
+//using std::ostream;
+
+string s;
+
+int main()
+{
+    string s2;
+
+    cout << s << endl;
+    cout << s2 << endl;
+    return 0;
+}
